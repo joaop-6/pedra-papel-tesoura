8111cbd7956191249934f38308e4a896bddf34d9
joaop-6 <122454808+joaop-6@users.noreply.github.com>
2023-01-11 13:20:21 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+let mao = 0
+input.onGesture(Gesture.Shake, function () {
+    let nao = 0
+    mao = randint(0, 2)
+    if (nao == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (nao == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
